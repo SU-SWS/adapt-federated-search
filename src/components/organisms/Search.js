4cd5564474273239d1e232475ebf50bd46aff996
Nathan Plowman <nathan.plowman@ffwagency.com>
2021-05-11 09:45:23 -0600

    ADAPT-2469: Created proof-of-concept for custom search redirects.

--- src/components/organisms/Search.js
+++ src/components/organisms/Search.js
@@ -2,8 +2,24 @@ import React from 'react'
 import { InstantSearch, SearchBox, Hits, RefinementList, Pagination, Configure, Stats} from 'react-instantsearch-dom'
 import SearchResultCard from '../cards/SearchResultCard'
 import algoliasearch from 'algoliasearch'
+
 const Search = ({appId, apiKey, indexName}) => {
-  const searchClient = algoliasearch(appId, apiKey)
+  const searchClient = algoliasearch(appId, apiKey);
+  const redirectIndex = searchClient.initIndex('custom_redirects');
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    // Redirect users for exact search phrases defined in custom_redirects index.
+    const searchPhrase = event.target[0].value;
+    redirectIndex.search('', {
+      filters: `searchPhrase:"${searchPhrase}"`
+    })
+    .then(({hits}) => {
+      if (hits.length && hits[0]['redirectURL']) {
+        window.location.href = hits[0]['redirectURL'];
+      }
+    })
+  }
   return (
     <div>
       <InstantSearch searchClient={searchClient} indexName={indexName} >
@@ -17,7 +33,7 @@ const Search = ({appId, apiKey, indexName}) => {
           </div>
           <div>
             <div className="mb-8">
-              <SearchBox />
+              <SearchBox onSubmit={submitHandler} />
               <Stats />
             </div>
             <Hits hitComponent={SearchResultCard} />
